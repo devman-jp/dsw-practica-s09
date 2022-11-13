3890246851b3d4f6edb98c7fa56ac04cc9602f29
DanielJ2006 <daniel_1_al@hotmail.com>
2022-11-12 21:26:01 -0500

    Alumno

--- src/main/java/practica/s09/model/Alumno.java
+++ src/main/java/practica/s09/model/Alumno.java
@@ -1,5 +1,40 @@
 package practica.s09.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import practica.s09.controller.Integer;
+import practica.s09.controller.String;
+@Entity
+@Table(name="alumno")
 public class Alumno {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idEstudiante;
+	private String nombre;
+	private String apellido;
+	
+	public Integer getIdEstudiante() {
+		return idEstudiante;
+	}
+	public void setIdEstudiante(Integer idEstudiante) {
+		this.idEstudiante = idEstudiante;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
 
 }
