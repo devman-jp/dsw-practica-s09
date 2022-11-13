cab2fdab860cf4f0f8a19297d01d472f130d5132
Jesus Toledo <jesusplasencia2508@hotmail.com>
2022-11-12 19:06:25 -0500

    Initial commit

---
+++ src/main/java/practica/s09/PracticaS09Application.java
@@ -0,0 +1,13 @@
+package practica.s09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaS09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaS09Application.class, args);
+	}
+
+}
---
+++ src/test/java/practica/s09/PracticaS09ApplicationTests.java
@@ -0,0 +1,13 @@
+package practica.s09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaS09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
